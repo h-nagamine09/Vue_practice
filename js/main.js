8fe822c6f597f9914ba69a6d83caaf9b82b445aa
Hiroki Nagamine <nagaminehiroki@hnimac.local>
2019-08-20 11:52:01 +0900

    Vue practice3

--- js/main.js
+++ js/main.js
@@ -7,12 +7,32 @@
   var vm = new Vue({
     el: '#app',
     data: {
-      // name:'taguchi'
-      todos: [
-        'task1',
-        'task2',
-        'task3'
-      ]
+      newItem: '',
+      todos: [{
+        title: 'task1',
+        isDone: false
+      }, {
+        title: 'task2',
+        isDone: false
+      }, {
+        title: 'task3',
+        isDone: true
+      }]
+    },
+    methods: {
+      addItem: function() {
+        var item = {
+          title: this.newItem,
+          isDone: false
+        };
+        this.todos.push(item);
+        this.newItem = '';
+      },
+      deleteItem: function(index) {
+        if (confirm('are you sure?')) {
+          this.todos.splice(index, 1);
+        }
+      }
     }
   });
 })();
